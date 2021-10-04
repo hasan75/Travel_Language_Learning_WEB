77af203b6ff04ac28df17931688835567f6ee7d0
Hasan75 <hasanjab14@gmail.com>
2021-10-04 06:20:42 +0600

    initial component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+              <Header></Header>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
