dd077575118b2ebe9d863add74b2f342cdb0c58b
Hasan75 <hasanjab14@gmail.com>
2021-10-04 06:31:35 +0600

    bootstrap imported in index.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Container, Nav, Navbar as Appbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div>
-            <h1>Header</h1>
+            <Appbar bg="dark" variant="dark">
+                <Container>
+                    <Appbar.Brand href="#home">Navbar</Appbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Appbar>
         </div>
     );
 };
